2c8f5be41adcb3c2138e3f57eb4bff2ed43687e1
gwillig <gustavwillig@gmail.com>
2022-01-10 19:42:06 +0100

    FEAT| h2 to show the reco text

---
+++ README.md
@@ -0,0 +1,4 @@
+Useful website
+
+* Useful examples: https://github.com/8thwall/web/blob/master/examples/aframe/animation-mixer/index.html
+* Visuals gltf: https://gltf-viewer.donmccurdy.com/
--- speech.js
+++ speech.js
@@ -3,7 +3,6 @@
 /*jshint strict:false */
 
 
-
 /// Variables for speech recognition
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
@@ -11,21 +10,23 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 //Add words from robot
-    // Animation of the robot
+// Animation of the robot
 const states = ['Idle', 'Walking', 'Running', 'Dance', 'Death', 'Sitting', 'Standing'];
 const emotes = ['Jump', 'Yes', 'No', 'Wave', 'Punch', 'ThumbsUp'];
-const mapping_robo = {"hallo":'Wave',"bye":'ThumbsUp',"tschüss":'ThumbsUp',"sitzen":"Sitting","stehen":"Standing",
-                  "tanze":'Dance', "rennen":"Running","spring":"Jump","ja":"Yes","böse":"Punch"}
+const mapping_robo = {
+    "hallo": 'Wave', "bye": 'ThumbsUp', "tschüss": 'ThumbsUp', "sitzen": "Sitting", "stehen": "Standing",
+    "tanze": 'Dance', "rennen": "Running", "spring": "Jump", "ja": "Yes", "böse": "Punch", "no": "No",
+    "tot":"Death"
+}
 
 
-
-function fillSelectMenu(){
+function fillSelectMenu() {
     const select = document.getElementById("selectMenu");
-    if (select.childElementCount<2){
+    if (select.childElementCount < 2) {
 
         var options = Object.keys(mapping_robo)
 
-        for(var i = 0; i < options.length; i++) {
+        for (var i = 0; i < options.length; i++) {
             var opt = options[i];
             var el = document.createElement("option");
             el.textContent = opt;
@@ -50,7 +51,6 @@ recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 
-
 function startSpeech() {
 
     const btn_speech = document.querySelector("#startSpeech")
@@ -61,7 +61,7 @@ function startSpeech() {
 
 }
 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
     // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
     // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
     // It has a getter so it can be accessed like an array
@@ -71,49 +71,117 @@ recognition.onresult = function(event) {
     // The second [0] returns the SpeechRecognitionAlternative at position 0.
     // We then return the transcript property of the SpeechRecognitionAlternative object
     var wordRaw = event.results[0][0].transcript;
-    const word =  wordRaw.toLowerCase();
-    const confidence = Math.round(event.results[0][0].confidence*100);
+    const word = wordRaw.toLowerCase();
+    const confidence = Math.round(event.results[0][0].confidence * 100);
 
-    console.log('Confidence: '+word+": " + event.results[0][0].confidence);
-    document.querySelector("#recoSpeech").textContent=`${word}: ${confidence} % `
-    let result = mapping_robo[word]
-    if (mapping_robo[word]==undefined){
-        result = 'No'
+    console.log('Confidence: ' + word + ": " + event.results[0][0].confidence);
+    document.querySelector("#recoSpeech").textContent = `${word}: ${confidence} % `
+    let animation = mapping_robo[word]
+    let emotion = {"morphtarget": "Sad", "value": 0}
+    let repetitions = 3
+
+    var vorstellen = ['vorstellen', "vorstellt", "verstellen"];
+    var goodbye = ["tschüss", "bye", "gut"]
+    var no = ["böse", "sauer", "gehen"]
+    var hello = ["hallo","hey"]
+    var das_ist = ["das", "ist"]
+    var clampWhenFinished = false
+    if (new RegExp(das_ist.join('|')).test(word)) {
+        repetitions = 1
+        animation = mapping_robo["tot"]
+        emotion = {"morphtarget": "Sad", "value": 0}
+        clampWhenFinished=true
+    }
+    else if (new RegExp(hello.join('|')).test(word)) {
+        repetitions = 3
+        animation = mapping_robo["hallo"]
+        emotion = {"morphtarget": "Sad", "value": 0}
+    }
+    else if (new RegExp(no.join('|')).test(word)) {
+        repetitions = 1
+        animation = mapping_robo["no"]
+        emotion = {"morphtarget": "Sad", "value": 2}
+    } else if (new RegExp(vorstellen.join('|')).test(word)) {
+        animation = mapping_robo["ja"]
+        emotion = {"morphtarget": "Surpise", "value": 4}
+    } else if (new RegExp(goodbye.join('|')).test(word)) {
+        animation = mapping_robo["tschüss"]
+    } else {
+        //If no word has been detected he will shake his head
+        repetitions = 3
+        animation = mapping_robo["no"]
+        emotion = {"morphtarget": "Sad", "value": 2}
     }
 
-    nextAnimation(result)
+    nextAnimation(animation, emotion, repetitions,clampWhenFinished)
 
 }
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
     recognition.stop();
     const btn_speech = document.querySelector("#startSpeech")
     btn_speech.classList.remove("blink_btn")
 }
 
-recognition.onnomatch = function(event) {
-  console.log(event)
+recognition.onnomatch = function (event) {
+    console.log(event)
 }
 
-recognition.onerror = function(event) {
-  console.log('Error occurred in recognition: ' + event.error)
+recognition.onerror = function (event) {
+    console.log('Error occurred in recognition: ' + event.error)
 }
 
-function nextAnimation(state) {
-    const model = document.getElementById('ar_object');
+function nextAnimation(animation, emotion, repetitions,clampWhenFinished) {
+    const model = document.getElementById('robot');
 
-     model.setAttribute('animation-mixer', {
-        clip:state,
+    model.setAttribute('animation-mixer', {
+        clip: animation,
         loop: "repeat",
-        repetitions:3,
-        timeScale:0.8,
+        repetitions: repetitions,
+        clampWhenFinished:clampWhenFinished,
+        timeScale: 0.8,
         crossFadeDuration: 0.4
 
-      });
-
+    });
+    model.setAttribute("gltf-morph", emotion)
 }
 
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+
+function onSelect(e) {
+    nextAnimation(mapping_robo[e])
+
+
+}
+
+
+//https://www.8thwall.com/8thwall/morph-targets-aframe/code/body.html
+const gltfMorphComponent = {
+    multiple: true,
+    schema: {
+        morphtarget: {type: 'string', default: ''},
+        value: {type: 'number', default: 0},
+    },
+    init() {
+        this.el.addEventListener('object3dset', () => {
+            this.morpher()
+        })
+    },
+    update() {
+        this.morpher()
+    },
+    morpher() {
+        const mesh = this.el.object3D
+        mesh.traverse((o) => {
+            if (o.morphTargetInfluences && o.userData.targetNames) {
+                const pos = o.userData.targetNames.indexOf(this.data.morphtarget)
+                o.morphTargetInfluences[pos] = this.data.value
+            }
+        })
+    },
+}
+AFRAME.registerComponent('gltf-morph', gltfMorphComponent)
